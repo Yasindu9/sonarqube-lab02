33f267d9abeb2feb9ddb49558667a774a1b1d115
Yasindu9 <144378880+Yasindu9@users.noreply.github.com>
2026-02-01 17:48:02 +0530

    Fix Calculator compile error

--- src/main/java/com/example/Calculator.java
+++ src/main/java/com/example/Calculator.java
@@ -1,47 +1,40 @@
-// EVEN WORSE: longer, more complex, duplicated logic
-public int calculate(int a, int b, String op) {
+package com.example;
 
-    if (op.equals("add")) {
-        return a + b;
+public class Calculator {
 
-    } else if (op.equals("add-again")) {
-        return a + b; // DUPLICATION
+    // (ඕනම විදිහකට "bad" version එක තිබ්බට build එක SUCCESS වෙන්න ඕන)
+    public int calculate(int a, int b, String op) {
 
-    } else if (op.equals("sub")) {
-        return a - b;
-
-    } else if (op.equals("sub-again")) {
-        return a - b; // DUPLICATION
-
-    } else if (op.equals("mul")) {
-        return a * b;
-
-    } else if (op.equals("div")) {
-
-        if (b == 0) {
-            return 0;
-        } else {
+        if (op.equals("add")) {
+            return a + b;
+        } else if (op.equals("add-again")) {
+            return a + b; // duplication intentional
+        } else if (op.equals("sub")) {
+            return a - b;
+        } else if (op.equals("sub-again")) {
+            return a - b; // duplication intentional
+        } else if (op.equals("mul")) {
+            return a * b;
+        } else if (op.equals("div")) {
+            if (b == 0) {
+                return 0;
+            }
             return a / b;
+        } else if (op.equals("mod")) {
+            return a % b;
+        } else if (op.equals("pow")) {
+            int result = 1;
+            for (int i = 0; i < b; i++) {
+                result = result * a;
+            }
+            return result;
+        } else {
+            return 0;
         }
-
-    } else if (op.equals("mod")) {
-        return a % b;
-
-    } else if (op.equals("pow")) {
-
-        int result = 1;
-
-        for (int i = 0; i < b; i++) {
-            result = result * a;
-        }
-
-        return result;
-
-    } else {
-        return 0;
     }
-}
-// INTENTIONAL DUPLICATION
-public int addAgain(int a, int b) {
-    return a + b;
+
+    // Change 2: add duplication method
+    public int addAgain(int a, int b) {
+        return a + b;
+    }
 }
